70016e97f35722f3f33903aa9dc16d18d53ff7b8
Gabriel Ilídio Souza De Menezes <57880411+gilidio8@users.noreply.github.com>
2021-04-29 16:00:27 -0300

    Create sobre.js

---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+
+function Sobre() {
+    return [
+        <div>
+            <h1>Hi' m Gabriel</h1>
+         <a href="/">Acessar página Inicial</a>   
+        </div>
+    ]
+}
+
+
+
+export default Sobre  
