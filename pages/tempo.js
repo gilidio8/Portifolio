a090d75a029f16f06f40dba48e7a561156e2533b
Gabriel Ilídio Souza De Menezes <57880411+gilidio8@users.noreply.github.com>
2021-04-29 17:56:33 -0300

    Create tempo.js

---
+++ pages/tempo.js
@@ -0,0 +1,25 @@
+function Tempo(props) {
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();
+
+    return (
+        <div>
+            <div>{dynamicDateString} (dinâmico)</div>
+                <div>{props.StaticDateString} (Estatico)</div> 
+        
+        </div>    
+    )
+}
+
+export function getStaticProps() {
+    const StaticDate = new Date();
+    const StaticDateString = StaticDate.toGMTString();
+
+    return {
+        props: {
+                StaticDateString
+        }
+    }    
+}
+
+export default Tempo;
