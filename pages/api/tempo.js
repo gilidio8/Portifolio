3932758ff382f60befc9239f9844cbb2198935a9
Gabriel Ilídio Souza De Menezes <57880411+gilidio8@users.noreply.github.com>
2021-04-30 10:35:39 -0300

    cache no tempo

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,6 +1,10 @@
-function tempo(request, response) {
+async function tempo(request, response) {
 
     const dynamicDate = new Date();
+
+    //const sbscribersResponse = await fetch();
+    
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
 
     response.json({
             date: dynamicDate.toGMTString()
