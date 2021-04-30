b59d5484711ed58b293cb1a019b68573eedb3ba5
Gabriel Ilídio Souza De Menezes <57880411+gilidio8@users.noreply.github.com>
2021-04-30 10:14:12 -0300

    erro na api resolvido

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,9 @@
-function tempo(request, response){
+function tempo(request, response) {
 
     const dynamicDate = new Date();
 
     response.json({
-            date: dynamicDate.toGMString()
+            date: dynamicDate.toGMTString()
     });
 }
 
